0fd6386a846d3c186645b958f9edcb05d8eb50d2
Александр Мельник <lokslu6@gmail.com>
2021-09-19 23:11:09 +0200

    Moving phrases to UserSetting.json file

--- Torpedo.Bot/Utils/Constants.cs
+++ Torpedo.Bot/Utils/Constants.cs
@@ -1,17 +1,37 @@
-﻿namespace Torpedo.Bot.Utils
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Torpedo.Bot.Utils
 {
     public class Constants
     {
+        static Constants()
+        {
+            string UserSetting = Path.Combine(Directory.GetCurrentDirectory(), "UserSetting.json");
+            string json;
+
+            using (StreamReader r = new StreamReader(UserSetting))
+            {
+                json = r.ReadToEnd();
+            }
+            JToken jObj = JToken.Parse(json);
+            jObj = jObj.SelectToken("Constans");
+
+            RESULT_FILE_NAME =(string)jObj.SelectToken("RESULT_FILE_NAME");
+            ERROR_UPLOAD_MESSAGE = (string)jObj.SelectToken("ERROR_UPLOAD_MESSAGE");
+            WHY_MESSAGE = (string)jObj.SelectToken("WHY_MESSAGE");
+            BOT_API_KEY = (string)jObj.SelectToken("BOT_API_KEY");
+        }
         // TODO: Input your message for video title, which will be converted and send to the chat
-        public const string RESULT_FILE_NAME = "";
+        public static readonly string RESULT_FILE_NAME;
 
         // TODO: Input your message for error converting/uploading message
-        public const string ERROR_UPLOAD_MESSAGE = "";
+        public static readonly string ERROR_UPLOAD_MESSAGE;
 
         // TODO: Input your message for non-text content in the direct messages with bot
-        public const string WHY_MESSAGE = "";
+        public static readonly string WHY_MESSAGE;
 
         // TODO: Input your telegram bot API key
-        public const string BOT_API_KEY = "";
+        public static readonly string BOT_API_KEY;
     }
 }
--- Torpedo.Bot/Utils/Phrases.cs
+++ Torpedo.Bot/Utils/Phrases.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Torpedo.Bot.Utils
 {
@@ -10,38 +15,51 @@ namespace Torpedo.Bot.Utils
 
         private static class Content
         {
+            static Content()
+            {
+                string UserSetting = Path.Combine(Directory.GetCurrentDirectory(), "UserSetting.json");
+                string json;
+
+                using (StreamReader r = new StreamReader(UserSetting))
+                {
+                    json = r.ReadToEnd();
+                }
+                JObject jObj = JObject.Parse(json);
+
+                var DirectPhrases = jObj.SelectToken("Content").SelectToken("DirectPhrases").ToString();
+                Content.DirectPhrases = JsonConvert.DeserializeObject<string[]>(DirectPhrases);
+
+                var CaptionPhrases = jObj.SelectToken("Content").SelectToken("CaptionPhrases").ToString();
+                Content.CaptionPhrases = JsonConvert.DeserializeObject<string[]>(CaptionPhrases);
+
+                var VoicePhrases = jObj.SelectToken("Content").SelectToken("VoicePhrases").ToString();
+                Content.VoicePhrases = JsonConvert.DeserializeObject<string[]>(VoicePhrases);
+
+
+            }
             public static string GetRandomCaption()
             {
                 var random = new Random();
-                return CaptionPhrases[random.Next(0, CaptionPhrases.Length - 1)];
+                return CaptionPhrases[random.Next(0, CaptionPhrases.Length )];
             }
 
             public static string GetRandomDirect()
             {
                 var random = new Random();
-                return DirectPhrases[random.Next(0, DirectPhrases.Length - 1)];
+                return DirectPhrases[random.Next(0, DirectPhrases.Length )];
             }
 
             public static string GetRandomVoice()
             {
                 var random = new Random();
-                return VoicePhrases[random.Next(0, VoicePhrases.Length - 1)];
+                return VoicePhrases[random.Next(0, VoicePhrases.Length )];
             }
 
-            private static readonly string[] DirectPhrases =
-            {
-                // TODO: Input phrases, which will be send to the client in direct chat with bot
-            };
+            private static readonly string[] DirectPhrases;
 
-            private static readonly string[] CaptionPhrases =
-            {
-                // TODO: Input phrases, which will be send in the video caption with converted video
-            };
+            private static readonly string[] CaptionPhrases;
 
-            private static readonly string[] VoicePhrases =
-            {
-                // TODO: Input phrases, which will be send in the video caption with converted video
-            };
+            private static readonly string[] VoicePhrases;
         }
     }
 }
